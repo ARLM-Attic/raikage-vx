20dc4e391c73ae59904431e3152b985b8796a6ff
SND\Ahmed1098_cp <SND\Ahmed1098_cp@0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5>
2014-09-05 11:55:59 +0000

    git-svn-id: https://raikagevx.svn.codeplex.com/svn@75363 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

---
+++ Raikage.Core/Aspects/Authentication/Authentication.cs
@@ -0,0 +1,18 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Serialization;
+using RaikageFramework.Base;
+using RaikageFramework.Base.Managers;
+
+namespace RaikageFramework.Aspects.Authentication
+{
+    [PSerializable]
+    public class Authentication : OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            if (!SecurityManager.IsAuthenticated)
+                ((BaseMvxViewModel)args.Instance).StartViewModel(SecurityManager.LoginType, null);
+            args.FlowBehavior = FlowBehavior.Return;
+        }
+    }
+}
---
+++ Raikage.Core/Base/Managers/SecurityManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaikageFramework.Base.Managers
+{
+    public static class SecurityManager
+    {
+        public static Type LoginType;
+        public static bool IsAuthenticated { get; set; }
+    }
+}
